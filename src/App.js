f2d5bacb9732848e11813fc57dfc63bb8cc48c8c
dylan <dylanabozarth@gmail.com>
2020-12-05 23:00:01 -0800

    fixed one issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+let allViews = Object.keys(Views).map((k) => Views[k]);
 
 const localizer = momentLocalizer(moment);
 const App = () => {
@@ -14,8 +15,8 @@ const App = () => {
       title: "Finish Calendar",
       start: moment(),
       end: moment(),
-      allDay: false
-    }
+      allDay: false,
+    },
   ]);
 
   const addEvent = (e) => {
@@ -23,58 +24,65 @@ const App = () => {
     newEvents.push({
       title: eventValue,
       start: moment().date(startValue),
-      end: moment().date(startValue)
-    });console.log(events)
+      end: moment().date(startValue),
+    });
+    console.log(events);
     setEvents(newEvents);
   };
-  const clearForm = () => { 
-    document.getElementById("form").reset();
-  }
-  const startOfMonth = moment().startOf('month').format('DD')
+
+  const startOfMonth = moment().startOf("month").format("DD");
 
   return (
     <div className="App">
-      
-      <div className="event-input">
-        <label for="eventInput">Event:&nbsp;</label>
-        <input
-          id="eventInput"
-          name="eventInput"
-          value={eventValue}
-          onChange={(e) => {
-            setEventValue(e.target.value);
-          }}
-        />
-        <label for="eventInput">Start&nbsp;</label>
-        <input
-          id="startInput"
-          name="startInput"
-          value={startValue}
-          onChange={(e) => {
-            
-           setStartValue(parseInt(startOfMonth) + parseInt(e.target.value) -1)
-          }}
-        />
-        <button onClick={addEvent}  >Add event</button>
+      <div className="text-center">
+        <div className="event-input">
+          <label for="eventInput">Event:</label>
+          <input
+            id="eventInput"
+            name="eventInput"
+            onChange={(e) => {
+              setEventValue(e.target.value);
+            }}
+          />
+          <br />
+          <label for="eventInput">Start:</label>
+          <input
+            id="startInput"
+            name="startInput"
+            onChange={(e) => {
+              setStartValue(
+                parseInt(startOfMonth) + parseInt(e.target.value) - 1
+              );
+            }}
+          />
+          <br />
+          <button onClick={addEvent}>Add event</button>
+        </div>
       </div>
       <Calendar
         localizer={localizer}
         defaultDate={new Date()}
         defaultView="month"
+        views={allViews}
         events={events}
-        style={{ height: "75vh" }}
+        style={{ height: "65vh" }}
       />
-      <div className="list">
-        <ul>
-          <li>Display Weekly view X</li>
-          <li>Highlight today X</li>
-          <li>Allow navigation to different weeks X</li>
-          <li>allow adding new events X</li>
-          <li>allow editing existing events</li>
-          <li>allow deleting events</li>
-          <li>Persisting data </li>
-          <li>Use apis to load and save data </li>
-        </ul>
+      <div className="row">
+        <div className="list col-sm-6">
+          <ul>
+            <li>Display Weekly view X</li>
+            <li>Highlight today X</li>
+            <li>Allow navigation to different weeks X</li>
+            <li>allow adding new events X</li>
+            <li>allow editing existing events</li>
+            <li>allow deleting events</li>
+            <li>Persisting data </li>
+            <li>Use apis to load and save data </li>
+          </ul>
+        </div>
+        <div className="col-sm-6">
+
+        </div>
       </div>
     </div>
   );
