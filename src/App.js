7e3b71177e10b264fee8976bcb0199b1440af776
dylan <dylanabozarth@gmail.com>
2020-12-06 22:24:28 -0800

    before changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 
@@ -9,56 +9,104 @@ let allViews = Object.keys(Views).map((k) => Views[k]);
 
 const localizer = momentLocalizer(moment);
 const App = () => {
-  const [eventValue, setEventValue] = useState("");
-  const [startValue, setStartValue] = useState("");
+  const [editEvent, setEditEvent] = useState(null);
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventStart, setEventStart] = useState("");
   const [events, setEvents] = useState([
     {
       id: uuidV4(),
       title: "Finish Calendar",
       start: moment(),
       end: moment(),
-      allDay: false,
-    },
+      allDay: false
+    }
   ]);
 
   const addEvent = (e) => {
-    const newEvents = [...events];
-    newEvents.push({
-      title: eventValue,
-      start: moment().date(startValue),
-      end: moment().date(startValue),
-    });
-   
-    setEvents(newEvents);
+    if (editEvent) {
+      setEvents((events) =>
+        events.map((event) =>
+          event.id === editEvent.id
+            ? {
+                ...event,
+                title: eventTitle,
+                start: eventStart,
+                end: eventStart
+              }
+            : event
+        )
+      );
+      setEditEvent(null);
+    } else {
+      setEvents((events) => [
+        ...events,
+        {
+          id: uuidV4(),
+          title: eventTitle,
+          start: moment().date(eventStart),
+          end: moment().date(eventStart)
+        }
+      ]);
+    }
+    setEventTitle("");
+    setEventStart("");
   };
- 
+
+  const selectEventHandler = (event, e) => setEditEvent(event);
+
+  const cancelEditMode = () => setEditEvent(null);
+
+  useEffect(() => {
+    if (editEvent) {
+      setEventTitle(editEvent.title);
+      setEventStart(editEvent.start);
+    }
+  }, [editEvent]);
+
+  useEffect(() => {
+    console.log("events", events);
+  }, [events]);
+
   const startOfMonth = moment().startOf("month").format("DD");
 
   return (
     <div className="App">
       <div className="text-center">
         <div className="event-input">
-          <label for="eventInput">Event:</label>
-          <input
-            id="eventInput"
-            name="eventInput"
-            onChange={(e) => {
-              setEventValue(e.target.value);
-            }}
-          />
+          <label>
+            Event:
+            <input
+              id="eventInput"
+              name="eventInput"
+              onChange={(e) => {
+                setEventTitle(e.target.value);
+              }}
+              value={eventTitle}
+            />
+          </label>
           <br />
-          <label for="eventInput">Start:</label>
-          <input
-            id="startInput"
-            name="startInput"
-            onChange={(e) => {
-              setStartValue(
-                parseInt(startOfMonth) + parseInt(e.target.value) - 1
-              );
-            }}
-          />
+          <label>
+            Start:
+            <input
+              id="startInput"
+              name="startInput"
+              onChange={(e) => {
+                setEventStart(
+                  parseInt(startOfMonth) + parseInt(e.target.value) - 1
+                );
+              }}
+              value={eventStart}
+            />
+          </label>
           <br />
-          <button onClick={addEvent}>Add event</button>
+          <button type="button" onClick={addEvent}>
+            {editEvent ? "Edit" : "Add"} event
+          </button>
+          {editEvent && (
+            <button type="button" onClick={cancelEditMode}>
+              Cancel
+            </button>
+          )}
         </div>
       </div>
       <Calendar
@@ -67,8 +115,8 @@ const App = () => {
         defaultView="month"
         views={allViews}
         events={events}
-        onSelectEvent={(event, e) => console.log('Selected event', event)}
         style={{ height: "65vh" }}
+        onSelectEvent={selectEventHandler}
       />
       <div className="row">
         <div className="list col-sm-6">
@@ -77,19 +125,15 @@ const App = () => {
             <li>Highlight today (X)</li>
             <li>Allow navigation to different weeks (X)</li>
             <li>allow adding new events (X)</li>
-            <li>allow editing existing events</li>
-            <li>allow deleting events </li>
+            <li>allow editing existing events (X)</li>
+            <li>allow deleting events (X) </li>
             <li>Persisting data (ran out of time)</li>
             <li>Use apis to load and save data (ran out of time)</li>
           </ul>
         </div>
-        <div className="col-sm-6">
         
-          
-        </div>
       </div>
     </div>
   );
 };
-
-export default App;
+export default App 
